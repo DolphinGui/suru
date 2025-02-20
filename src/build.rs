868bc18b50c39f2bee32d756785b333764437ae4
Shin Umeda <umeda.shin@gmail.com>
2025-02-20 14:45:27 -0800

    This can now parse and navigate build graphs

--- src/build.rs
+++ src/build.rs
@@ -2,27 +2,47 @@ use std::{
     collections::HashMap,
     fmt::Debug,
     path::Path,
-    sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard},
+    sync::{Arc, Once, RwLock, RwLockReadGuard, RwLockWriteGuard},
+    thread::Thread,
+    time::Duration,
 };
 
 use threadpool::ThreadPool;
 
-use crate::parser::{BldFile, Task};
+use crate::{
+    parser::{BldFile, Recipe, Task},
+    util::remove_prefix,
+};
 
 #[derive(Debug)]
 struct Target {
     dependencies: Vec<String>,
-    dependents: Vec<DynTarget>,
+    dependents: Vec<Dependent>,
     is_branch: bool,
 }
 
-type DynTarget = Arc<RwLock<Target>>;
+type DynTarget = RwLock<Target>;
+type Dependent = Arc<(String, DynTarget, Once)>;
 
 pub fn compile(input: BldFile) {
     let roots = get_roots(input.tasks);
 
     println!("{} roots: {:?}", roots.len(), roots);
-    // let runner = ThreadPool::new(threads);
+    let runner = Box::leak(Box::new(ThreadPool::new(num_cpus::get_physical()))) as &ThreadPool;
+
+    let recipes = Box::leak(Box::new(input.recipes)) as &HashMap<String, Vec<Recipe>>;
+
+    for arc in &roots {
+        let arc = arc.1.clone();
+        runner.execute(move || {
+            if !arc.2.is_completed() {
+                arc.2.call_once(|| {
+                    build_deps(arc.clone(), recipes, runner);
+                });
+            }
+        });
+    }
+    runner.join();
 }
 
 fn write(d: &DynTarget) -> RwLockWriteGuard<'_, Target> {
@@ -32,25 +52,29 @@ fn read(d: &DynTarget) -> RwLockReadGuard<'_, Target> {
     d.read().expect("This section is single threaded")
 }
 
-fn get_roots(tasks: HashMap<String, Task>) -> Vec<(String, DynTarget)> {
-    let unprocessed: HashMap<String, DynTarget> =
+fn get_roots(tasks: HashMap<String, Task>) -> Vec<(String, Dependent)> {
+    let unprocessed: HashMap<String, Dependent> =
         HashMap::from_iter(tasks.into_iter().map(|(file, deps)| {
             (
-                file,
-                Arc::new(RwLock::new(Target {
-                    dependencies: deps.inputs.into_iter().collect(),
-                    dependents: Default::default(),
-                    is_branch: false,
-                })),
+                file.clone(),
+                Arc::new((
+                    file,
+                    RwLock::new(Target {
+                        dependencies: deps.inputs.into_iter().collect(),
+                        dependents: Default::default(),
+                        is_branch: false,
+                    }),
+                    Once::new(),
+                )),
             )
         }));
 
     for (_, target_deps) in &unprocessed {
-        let mut td = write(target_deps);
+        let mut td = write(&target_deps.1);
         let mut is_branch = false;
         for dep in td.dependencies.iter() {
             if let Some(d) = unprocessed.get(dep) {
-                write(d).dependents.push(target_deps.clone());
+                write(&d.1).dependents.push(target_deps.clone());
                 is_branch = true;
             }
         }
@@ -61,6 +85,49 @@ fn get_roots(tasks: HashMap<String, Task>) -> Vec<(String, DynTarget)> {
 
     unprocessed
         .into_iter()
-        .filter(|(_, b)| !read(b).is_branch)
+        .filter(|(_, b)| !read(&b.1).is_branch)
         .collect()
+}
+
+fn read_s(d: &DynTarget) -> RwLockReadGuard<'_, Target> {
+    d.read().expect("This section is read only")
+}
+
+fn build_deps(
+    target: Dependent,
+    recipes: &'static HashMap<String, Vec<Recipe>>,
+    runner: &'static ThreadPool,
+) {
+    let a = recipes.get(&remove_prefix(&target.0));
+    if let Some(rs) = a {
+        run_recipe(&target.0, &read_s(&target.1).dependencies, rs);
+    } else {
+        panic!(
+            "Could not find a recipe to build {}\n Recipes: {:?}",
+            target.0, recipes
+        )
+    }
+
+    std::thread::sleep(Duration::from_millis(100));
+
+    let target = read_s(&target.1);
+    for arc in &target.dependents {
+        let arc = arc.clone();
+        runner.execute(move || {
+            if !arc.2.is_completed() {
+                arc.2.call_once(|| {
+                    build_deps(arc.clone(), recipes, runner);
+                });
+            }
+        });
+    }
+}
+
+fn run_recipe(filename: &str, dependencies: &[String], recipes: &[Recipe]) {
+    let a = recipes.iter().find(|r| {
+        r.inputs
+            .iter()
+            .any(|input| dependencies.iter().any(|dep| dep.contains(input)))
+    });
+    println!("recipe found for {}: {:?}", filename, a);
 }
